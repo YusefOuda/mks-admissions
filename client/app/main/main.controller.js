e6721fee462da4ebdeb49200b2192a1e4c0f2622
Brian Patterson <brianpatterson2013@gmail.com>
2014-11-10 20:55:08 -0600

    Added instructions for first challenge question, begun scaffolding, and removed boilerplate code.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('admissionsApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope) {
     
-    $scope.editor = ace.edit("editor");
-    $scope.editor.getSession().setMode("ace/mode/javascript");
+    $scope.editor = ace.edit('editor');
+    $scope.editor.getSession().setMode('ace/mode/javascript');
 
     $scope.submit = function() {
       console.log($scope.editor.getValue());
