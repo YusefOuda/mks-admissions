e4bd7877163147a3aa499a1a5292d59b2fd9dd97
Yusef Ouda <yiouda92@gmail.com>
2014-11-11 11:09:48 -0600

    add function which checks if the user's answer is correct or not and assigns a value either way.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,20 +3,20 @@
 angular.module('admissionsApp')
   .controller('MainCtrl', function ($scope, $http) {
     
-    $scope.editor = ace.edit("editor");
-    $scope.editor.getSession().setMode("ace/mode/javascript");
+    $scope.editor = ace.edit('editor');
+    $scope.editor.getSession().setMode('ace/mode/javascript');
 
     $scope.submit = function() {
       var code = $scope.editor.getValue();
 
       var wrapCode = function (code) {
-        var result = code + ";\ntry {";
-        result += "postMessage({ answer: getContactInfo() });";
-        result += "} catch (e) {" +
-          "var trace = printStackTrace({e: e});" +
-          "postMessage({ error: { name: e.name, message: e.message}, trace: trace });" +
-        "}";
-        result += "; postMessage('__terminate__');\n";
+        var result = code + ';\ntry {';
+        result += 'postMessage({ answer: getContactInfo() });';
+        result += '} catch (e) {' +
+          'var trace = printStackTrace({e: e});' +
+          'postMessage({ error: { name: e.name, message: e.message}, trace: trace });' +
+        '}';
+        result += '; postMessage("__terminate__");\n';
         return result;
       };
 
@@ -27,11 +27,28 @@ angular.module('admissionsApp')
           'data:application/javascript,' + encodeURIComponent(wrapCode(code))
         ],
         callback: function(data, error) {
-          if (data && data.error)
-            console.log("error: ", data.error);
-          if (data && data.answer)
-            console.log("data: ", data.answer);
+          if (data && data.error) {
+            $scope.syntaxError = data.error;
+          }
+          if (data && data.answer) {
+            $scope.userAnswer = data.answer;
+            checkCorrectness($scope.userAnswer);
+          }
         }
       });
+
+      var checkCorrectness = function(answer) {
+        var objectLength = Object.keys(answer).length;
+        if (objectLength === 4) {
+          if (answer.hasOwnProperty('fullName') && answer.hasOwnProperty('email') 
+            && answer.hasOwnProperty('skype') && answer.hasOwnProperty('github')) {
+            $scope.isCorrect = true;
+          } else {
+            $scope.isCorrect = false;
+          }
+        } else {
+          $scope.isCorrect = false;
+        }
+      };
     };
   });
